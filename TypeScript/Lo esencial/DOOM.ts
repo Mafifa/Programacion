5f945e4438d2a10cb66fec975c7da598f5c6d7c7
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-01-20 17:42:56 -0400

    Se agrego como se debe de trabajar con el DOM

--- TypeScript/Lo esencial/DOOM.ts
+++ TypeScript/Lo esencial/DOOM.ts
@@ -1,1 +1,18 @@
 //DOM CON TYPESCRIPT
+const canvas = document.getElementById("canvas");
+
+// SE INFIERE QUE DENTRO DEL IF YA SOLO QUEDA REPORAR UN HTMLCanvasElement
+if (canvas instanceof HTMLCanvasElement) {
+  // <---- Inferencia
+  const ctx = canvas.getContext("2d");
+}
+
+function mostrarLongitud(objecto: number | string) {
+  if (typeof objecto === "string") {
+    return objecto.length;
+  }
+
+  return objecto.toString().length;
+}
+
+mostrarLongitud(1);
