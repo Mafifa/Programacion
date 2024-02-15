4632be40755e02cb4f286c595b96e86b7c88add3
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-02-14 23:28:39 -0400

    Mejoras escalables

--- JavaScript/Guia de API/API-GUIA/src/App.jsx
+++ JavaScript/Guia de API/API-GUIA/src/App.jsx
@@ -1,15 +1,48 @@
-import response from "./assets/mock/response.json";
-import "./index.css";
+import { useRef } from "react";
+import { useHero, heros } from "./hooks/useHero";
 
 function App() {
-  const HEROS = response.results;
-  const HAS_HERO = HEROS?.length > 0;
+  const { hero: mappedHero } = useHero();
+  const inputRef = useRef();
+  const HAS_HEROS = heros?.length > 0;
 
+  const handleSubbmit = () => {
+    const value = inputRef.current.value;
+    console.log(value);
+  };
+  function heroRender() {
+    return (
+      <ul className="max-w-5xl flex flex-wrap mx-auto">
+        {mappedHero.map((hero) => (
+          <li className="flex bg-slate-900 m-4 rounded-md" key={hero.id}>
+            <div className="mr-4">
+              <img
+                src={hero.image}
+                alt={hero.nameHero}
+                className="max-w-32 rounded-md"
+              />
+            </div>
+            <div className="p-4">
+              <p>{hero.nameHero}</p>
+              <p>{hero.realName}</p>
+              <p>{hero.work}</p>
+              <p>{hero.pubisher}</p>
+              <p>{hero.race}</p>
+              <p>{hero.gender}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  function noResponse() {
+    return <p>No se encontraron heroes</p>;
+  }
   return (
     <div className="text-white">
       <header className="flex flex-col items-center max-w-5xl mx-auto">
         <h1 className="m-10 text-4xl font-bold">Buscador de Heroes</h1>
-        <form action="">
+        <form onSubmit={handleSubbmit}>
           <label htmlFor="name" className="m-4">
             Busca un heroe
           </label>
@@ -17,36 +50,22 @@ function App() {
             placeholder="Batman, Ironman..."
             type="text"
             id="name"
+            ref={inputRef}
             name="name"
+            className="text-black"
             required
           />
-          <button className="bg-blue-500 m-3 p-2 rounded-xl font-bold">
-            buscar
+          <button
+            type="button"
+            onClick={handleClick}
+            className="bg-blue-500 m-3 p-2 rounded-xl font-bold"
+          >
+            screch
           </button>
         </form>
       </header>
       <main>
-        {HAS_HERO ? (
-          <ul className="max-w-5xl flex flex-wrap mx-auto">
-            {HEROS.map((hero) => (
-              <li className="flex bg-slate-900 m-4 rounded-md" key={hero.id}>
-                <div className="mr-4">
-                  <img
-                    src={hero.image.url}
-                    alt={hero.name}
-                    className="max-w-32 rounded-md"
-                  />
-                </div>
-                <div className="p-4">
-                  <h3>{hero.name}</h3>
-                  <p>{hero.biography["full-name"]}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>NO SE ENCONTRARON HEROES</p>
-        )}
+        <div>{HAS_HEROS ? heroRender() : noResponse()}</div>
       </main>
     </div>
   );
---
+++ JavaScript/Guia de API/API-GUIA/src/hooks/useHero.js
@@ -0,0 +1,16 @@
+import responseJSON from "../assets/mock/response.json";
+export const heros = responseJSON.results;
+export function useHero() {
+  const mappedHero = heros?.map((hero) => ({
+    id: hero.id,
+    nameHero: hero.name,
+    realName: hero.biography["full-name"],
+    work: hero.work.occupation,
+    pubisher: hero.biography.publisher,
+    gender: hero.appearance.gender,
+    race: hero.appearance.race,
+    image: hero.image.url,
+  }));
+
+  return { hero: mappedHero };
+}
